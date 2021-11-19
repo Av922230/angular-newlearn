b6c91ebb45adaf29f98a6d5a9847beaa937db6ae
Av922230 <av922230@gmail.com>
2021-11-19 11:12:33 +0530

    2 files modified, 12 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,19 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import {EmployeeClass } from './EmployeeDetails/Employee.component';
-import {TwoWayDataBinding} from './TwoWayDataBinding/TwoWayComponent';
+import { EmployeeClass } from './EmployeeDetails/Employee.component';
+import { TwoWayDataBinding } from './TwoWayDataBinding/TwoWayComponent';
+import { CommonServiceClass } from './SMC/CommonService';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent,EmployeeClass,TwoWayDataBinding ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    EmployeeClass,
+    TwoWayDataBinding,
+    CommonServiceClass,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

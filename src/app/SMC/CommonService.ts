de25449ebbaf06ba6d344bd82a3b596eb3371b6b
Av922230 <av922230@gmail.com>
2021-11-19 16:20:53 +0530

    2 files modified

--- src/app/SMC/CommonService.ts
+++ src/app/SMC/CommonService.ts
@@ -1,15 +1,13 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector:'CommonService',
-  templateUrl:'../SMC/CommonService.html'
+  selector: 'CommonService',
+  templateUrl: '../SMC/CommonService.html',
 })
-
-export class CommonServiceClass{
-  public Price1 : number = 0;
-  public Unit1 : number =0;
-
-  public Price2:number =0;
-  public Unit2:number =0;
-
+export class CommonServiceClass {
+  public Price1: number = 0;
+  public Unit1: number = 0;
+  public Price2: number = 0;
+  public Unit2: number = 0;
+  public clickCount :number = 0;
 }
